f9722900254b110c1e0d0e85c46c0a6889616c95
Elia <eliapym@hotmail.co.uk>
2026-05-28 23:23:02 +0100

    Configure Terraform provider and local backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version ="~> 5.0"
+    }
+  }
+
+  backend "local" {
+    path = ".terraform/terraform.tfstate"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
