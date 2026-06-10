2599891a3c48f790d7d97dece2878a5cc1c1ed29
Elia Pym <elia.pym@e-p.dev>
2026-06-10 16:43:16 +0100

    Setting outputs
    After running `terraform apply` it will show the outputs for the API Gateway endpoint and S3 bucket to host frontend.

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,14 @@
+output "api_gateway_endpoint" {
+    description = "Public URL of backend"
+    value = aws_apigatewayv2_api.url_shortner_api.api_endpoint
+}
+
+output "frontend_s3_bucket_name" {
+    description = "Name of S3 bucket for hosting frontend"
+    value = aws_s3_bucket.url_shortener_frontend.id
+}
+
+output "frontend_s3_website_url" {
+    description = "URL of S3 buckte for frontend"
+    value = aws_s3_bucket_website_configuration.url_shortener_frontend.website_endpoint
+}
