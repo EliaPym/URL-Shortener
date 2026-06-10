aa1598c4f3f783572acabe4f74076dfc37ce98b4
Elia Pym <elia.pym@e-p.dev>
2026-06-10 16:38:58 +0100

    Added API Gateway
    Defining AWS API Gateway resources, including resources for lambda integration and defaults.

---
+++ infrastructure/api.tf
@@ -0,0 +1,36 @@
+resource "aws_apigatewayv2_api" "url_shortner_api" {
+    name = "url_shortner_api"
+    protocol_type = "HTTP"
+    
+    tags = {
+        Environment = "Production"
+        Project = var.project_name
+    }
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+    api_id = aws_apigatewayv2_api.url_shortner_api.id
+    integration_type = "AWS_PROXY"
+    integration_method = "POST"
+    integration_uri = aws_lambda_function.url_shortener_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+    api_id = aws_apigatewayv2_api.url_shortner_api.id
+    route_key = "ANY /{proxy+}"
+    target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+    api_id = aws_apigatewayv2_api.url_shortner_api.id
+    name = "$default"
+    auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gw_invoke" {
+    statement_id = "AllowExecutionFromAPIGateway"
+    action = "lambda:InvokeFunciton"
+    function_name = aws_lambda_function.url_shortener_lambda.function_name
+    principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_apigatewayv2_api.url_shortener_api.execution_arn}/*/*"
+}
